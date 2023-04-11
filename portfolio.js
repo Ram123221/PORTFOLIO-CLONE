d3ca870688d588a5c427f8298485cd4639b82aa6
Ram123221 <ramgrg1024@gmail.com>
2023-04-11 07:21:25 +0545

    few changes made

--- portfolio.js
+++ portfolio.js
@@ -16,3 +16,26 @@ toggleBtn.onclick = function()
         links.classList.remove("show");
     }
 }
+
+const body = document.querySelector("body");
+const headerNavSticky = document.getElementById("header-n-nav-sticky");
+const lightMode = document.querySelector("span.light");
+const darkMode = document.querySelector("span.dark");
+
+lightMode.onclick = function(){
+    lightMode.classList.remove("visible");
+    darkMode.classList.add("visible");
+    lightMode.classList.add("invisible");
+    darkMode.classList.remove("invisible");
+    body.classList.remove("black-white");
+    headerNavSticky.classList.remove("black-white");
+}
+
+darkMode.onclick = function(){
+    lightMode.classList.add("visible");
+    darkMode.classList.remove("visible");
+    lightMode.classList.remove("invisible");
+    darkMode.classList.add("invisible");
+    body.classList.add("black-white");
+    headerNavSticky.classList.add("black-white");
+}
